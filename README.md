46ae5f681d58ceb53631bb630e7cc2e35ea6109e
Robert McClintick <robert.mcc3@gmail.com>
2022-05-23 19:13:58 -0400

    coded game loop. update README.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # RockPaperScissors
-Javascript application for playing a single round of Rock, Paper, Scissors as outlined in The Odin Project's assignment here
+Javascript application for playing a single game of Rock, Paper, Scissors as outlined in The Odin Project's assignment here
 
 https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
 
 
-Played fully in the console
+Played fully in the console. Each game is best of 5 rounds.
--- rps.js
+++ rps.js
@@ -1,3 +1,7 @@
+let userScore = 0;
+let computerScore = 0;
+
+//returns a randomly selected choice from {Rock, Paper, Scissors}
 function computerPlay()
 {
     let rand = Math.floor(Math.random() * 3);
@@ -18,28 +22,45 @@ function playRound(playerSelection, computerSelection)
         return `Draw! You both played ${playerSelection}`;
     }
     //win cases
-    else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase()==='Scissors')
+    else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase()==='SCISSORS')
     {
+        userScore ++;
         return 'You win! Rock beats Scissors!';
     }
-    else if (playerSelection.toUpperCase() === 'Paper' && computerSelection.toUpperCase()==='Rock')
+    else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection.toUpperCase()==='ROCK')
     {
+        userScore ++;
         return 'You win! Paper beats Rock!';
     }
-    else if (playerSelection.toUpperCase() === 'Scissors' && ccomputerSelection.toUpperCase()==='Paper')
+    else if (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection.toUpperCase()==='PAPER')
     {
+        userScore ++;
         return 'You win! Scissors beats Paper!';
     }
     //lose case
     else 
     {
+        computerScore ++;
         return `You Lose! ${computerSelection} beats ${playerSelection}!`;
     }
 }
 
-//test cases
-console.log(playRound('Rock', 'Paper'));
-console.log(playRound('Paper', 'Paper'));
-console.log(playRound('scissors', 'Paper'));
-console.log(playRound('paper', 'Scissors'));
-console.log(playRound('Rock', 'Paper'));
+function game()
+{
+    //game loop. plays 5 rounds
+    for (let i = 0; i < 5; i++)
+    {
+        let userChoice = prompt('Rock, Paper, or Scissors');
+        console.log(playRound(userChoice, computerPlay()));
+    }
+    //display scores and check if player won
+    console.log(`Your score: ${userScore} \tComputer's score: ${computerScore}`);
+    if (userScore > computerScore) 
+        console.log('You Win!');
+    else if (userScore == computerScore)
+        console.log('Draw');
+    else 
+        console.log('You Lose!');
+}
+
+game();
