23c3d57b912eba62c15f3360b905d233f60ca0d6
Robert McClintick <robert.mcc3@gmail.com>
2022-05-23 18:57:38 -0400

    coded/tested playRound function

--- rps.js
+++ rps.js
@@ -10,6 +10,36 @@ function computerPlay()
     }
 }
 
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection)
+{
+    //draw case
+    if (playerSelection.toUpperCase() === computerSelection.toUpperCase())
+    {
+        return `Draw! You both played ${playerSelection}`;
+    }
+    //win cases
+    else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase()==='Scissors')
+    {
+        return 'You win! Rock beats Scissors!';
+    }
+    else if (playerSelection.toUpperCase() === 'Paper' && computerSelection.toUpperCase()==='Rock')
+    {
+        return 'You win! Paper beats Rock!';
+    }
+    else if (playerSelection.toUpperCase() === 'Scissors' && ccomputerSelection.toUpperCase()==='Paper')
+    {
+        return 'You win! Scissors beats Paper!';
+    }
+    //lose case
+    else 
+    {
+        return `You Lose! ${computerSelection} beats ${playerSelection}!`;
+    }
+}
+
+//test cases
+console.log(playRound('Rock', 'Paper'));
+console.log(playRound('Paper', 'Paper'));
+console.log(playRound('scissors', 'Paper'));
+console.log(playRound('paper', 'Scissors'));
+console.log(playRound('Rock', 'Paper'));
